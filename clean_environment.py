da722021c9d8958dd6dfe5d1f6185f1636612e42
StephanGessel <51866321+StephanGessel@users.noreply.github.com>
2021-02-27 19:12:31 +0200

    Update clean_environment.py

--- clean_environment.py
+++ clean_environment.py
@@ -5,7 +5,6 @@ def close_servers():
 
     try:
         requests.get('http://127.0.0.1:5000/stop_server')
-        requests.get('http://127.0.0.1:5001/stop_server')
         print('Servers closed!')
     except Exception as e:
         print(e)
