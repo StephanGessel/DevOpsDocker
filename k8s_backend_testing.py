8aacbdbde40e97322a896e8ce044329c2ae13824
StephanGessel <51866321+StephanGessel@users.noreply.github.com>
2021-03-08 15:45:13 +0200

    Create k8s_backend_testing.py

---
+++ k8s_backend_testing.py
@@ -0,0 +1,10 @@
+import requests
+
+try:
+    with open("k8s_url.txt", "r") as f:
+        url = f.read()    
+    res = requests.get(f'{url}/users/1')
+    print(res.json())
+    
+except Exception as e:
+    print(e)
